768a82f5f34568d63e899527a4221f89846b49c0
subas <subasmohanty256@gmail.com>
2025-12-12 23:46:00 +0530

    refactor: used updater function to update state values

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,15 +7,13 @@ import { Button } from "neetoui";
 const Carousel = ({ imageUrls, title }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const handleNext = () => {
-    const nextIndex = (currentIndex + 1) % imageUrls.length;
-    setCurrentIndex(nextIndex);
-  };
+  const handleNext = () =>
+    setCurrentIndex(prevIndex => (prevIndex + 1) % imageUrls.length);
 
-  const handlePrevious = () => {
-    const prevIndex = (currentIndex - 1 + imageUrls.Length) % imageUrls.length;
-    setCurrentIndex(prevIndex);
-  };
+  const handlePrevious = () =>
+    setCurrentIndex(
+      prevIndex => (prevIndex - 1 + imageUrls.length) % imageUrls.length
+    );
 
   return (
     <div className="flex flex-col items-center">
