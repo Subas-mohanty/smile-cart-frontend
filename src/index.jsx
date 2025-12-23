c6a0597cf6de77aefdf2324db2c7fd06aef35b7a
subas <subasmohanty256@gmail.com>
2025-12-23 21:20:47 +0530

    chore: Initialized i18n for language translation

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
+import "./common/i18n";
+//i18n should load before App initialization. Hence, disabling import/order rule. (this below line disable the eslint rule for the very next line)
+// eslint-disable-next-line import/order
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
