bbd48b32bbbb1a9ef41c59b64c7a364d3544d5d6
subas <subasmohanty256@gmail.com>
2025-12-24 22:38:17 +0530

    feat: adjust productList component to use query parameter info

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -3,28 +3,57 @@ import { useState } from "react";
 import { Header, PageLoader } from "components/commons/";
 import { useFetchProducts } from "hooks/reactQuery/useProductsApi";
 import useDebounce from "hooks/useDebounce";
+import useFuncDebounce from "hooks/useFuncDebounce";
+import useQueryParams from "hooks/useQueryParams";
+import { filterNonNull } from "neetocist";
 import { Search } from "neetoicons";
 import { Input, NoData, Pagination } from "neetoui";
-import { isEmpty } from "ramda";
+import { isEmpty, mergeLeft } from "ramda";
 import { useTranslation } from "react-i18next";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import routes from "routes";
+import { buildUrl } from "utils/url";
 
 import { DEFAULT_PAGE_INDEX, DEFAULT_PAGE_SIZE } from "./constants";
 import ProductListItem from "./ProductListItem";
 
 const ProductList = () => {
   const { t } = useTranslation();
+  const history = useHistory();
 
   // const [products, setProducts] = useState([]);
   // const [isLoading, setIsLoading] = useState(true);
-  const [searchValue, setSearchValue] = useState("");
-  const [currentPage, setCurrentPage] = useState(DEFAULT_PAGE_INDEX);
 
-  const debouncedSearchKey = useDebounce(searchValue);
+  const queryParams = useQueryParams();
+  const { page, pageSize, searchTerm = "" } = queryParams;
+  const [searchKey, setSearchKey] = useState(searchTerm);
+
+  const debouncedSearchKey = useDebounce(searchKey);
+
+  // Notice we are not calling the function or passing the page parameter to the handlePageNavigation function inside the pagination component, then how it is being used here
+  // the reason is Pagination component internally calls the handlePageNavigation function with the current page when the current page changes
+  const handlePageNavigation = page =>
+    history.replace(
+      buildUrl(
+        routes.products.index,
+        mergeLeft({ page, pageSize: DEFAULT_PAGE_SIZE }, queryParams)
+      )
+    );
+
+  const updateQueryParams = useFuncDebounce(value => {
+    const params = {
+      page: DEFAULT_PAGE_INDEX,
+      pageSize: DEFAULT_PAGE_SIZE,
+      searchTerm: value || null,
+    };
+
+    history.replace(buildUrl(routes.products.index, filterNonNull(params)));
+  });
 
   const productsParams = {
     searchTerm: debouncedSearchKey,
-    page: currentPage,
-    pageSize: DEFAULT_PAGE_SIZE,
+    page: Number(page),
+    pageSize: Number(pageSize),
   };
 
   const { data: { products = [], totalProductsCount } = {}, isLoading } =
@@ -43,10 +72,10 @@ const ProductList = () => {
               placeholder={t("searchProducts")}
               prefix={<Search />}
               type="search"
-              value={searchValue}
-              onChange={event => {
-                setSearchValue(event.target.value);
-                setCurrentPage(DEFAULT_PAGE_INDEX);
+              value={searchKey}
+              onChange={({ target: { value } }) => {
+                updateQueryParams(value);
+                setSearchKey(value);
               }}
             />
           }
@@ -64,9 +93,9 @@ const ProductList = () => {
       <div className="self-end pb-3">
         <Pagination
           count={totalProductsCount}
-          navigate={page => setCurrentPage(page)}
-          pageNo={currentPage || DEFAULT_PAGE_INDEX}
-          pageSize={DEFAULT_PAGE_SIZE}
+          navigate={handlePageNavigation}
+          pageNo={Number(page) || DEFAULT_PAGE_INDEX}
+          pageSize={Number(pageSize)}
         />
       </div>
     </div>
